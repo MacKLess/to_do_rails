3a5ca1f2c37900b0f5a9886549fdede563bd6e0f
LD MacKrell <ldmackrell@gmail.com>
2017-11-15 00:59:18 -0800

    Update app to pass test of adding tasks.

--- spec/features/edit_task_pages_spec.rb
+++ spec/features/edit_task_pages_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
+describe "the add a task process" do
+  it "adds a new task" do
+    list = List.create(:name => 'Home stuff')
+    visit list_path(list)
+    click_link "Add a task"
+    # task = Task.create(:description => 'Laundry')
+    fill_in 'Description', :with => 'Laundry'
+    click_on 'Create Task'
+    visit list_path(list)
+    expect(page).to have_content 'Laundry'
+  end
+
+  it "gives error when no description is entered" do
+    list = List.create(:name => 'Home stuff')
+    visit list_path(list)
+    click_link "Add a task"
+    # task = Task.create(:description => 'Laundry')
+    fill_in 'Description', :with => ''
+    click_on 'Create Task'
+    expect(page).to have_content 'error'
+  end
+end
+
 describe "the edit a task process" do
   it "edits a task" do
     list = List.create(:name => 'Home stuff')
