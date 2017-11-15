dfe1e49614f78cea5f0546b6c351c424cb9e04ad
LD MacKrell <ldmackrell@gmail.com>
2017-11-14 23:58:30 -0800

    Update app with test for editing a list.

--- spec/features/add_list_pages_spec.rb
+++ spec/features/add_list_pages_spec.rb
@@ -15,4 +15,17 @@ describe "the add a list process" do
     expect(page).to have_content 'errors'
   end
 
+  describe "the edit a list process" do
+    it "edits a list" do
+      list = List.create(:name => 'Home stuff')
+      visit lists_path
+      click_link 'Home stuff'
+      click_link 'Edit List'
+      fill_in 'Name', :with => 'Homework stuff'
+      click_on 'Update List'
+      visit list_path(list)
+      expect(page).to have_content 'Homework stuff'
+    end
+  end
+
 end
